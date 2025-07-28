cdf2805e835e290a6f9f11a7c94f3e3b1907e925
Ronivaldo de Macedo <Roni_Trabalho@hotmail.com>
2025-07-28 19:22:57 -0300

    criando o projeto

---
+++ MyNotes/app/src/main/java/com/example/mynotes/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.mynotes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.mynotes.ui.theme.MyNotesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyNotesTheme {
+
+            }
+        }
+    }
+}
